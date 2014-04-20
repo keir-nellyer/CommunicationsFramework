068cda6ef2fc644a9b16a9c3eaef77ead17e285c
Keir Nellyer <keir@nellyer.co.uk>
2014-04-20 14:07:29 +0100

    Small changes

--- src/main/java/com/iKeirNez/packetapi/implementation/connection/IClientConnection.java
+++ src/main/java/com/iKeirNez/packetapi/implementation/connection/IClientConnection.java
@@ -1,19 +1,12 @@
 package com.iKeirNez.packetapi.implementation.connection;
 
 import com.iKeirNez.packetapi.api.connection.ClientConnection;
-import com.iKeirNez.packetapi.implementation.handlers.ConnectionHandler;
-import com.iKeirNez.packetapi.implementation.handlers.ReconnectHandler;
+import com.iKeirNez.packetapi.implementation.handlers.StandardInitializer;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelInitializer;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
-import io.netty.handler.codec.serialization.ClassResolvers;
-import io.netty.handler.codec.serialization.ObjectDecoder;
-import io.netty.handler.codec.serialization.ObjectEncoder;
-import io.netty.handler.timeout.IdleStateHandler;
 
 import java.util.concurrent.TimeUnit;
 
@@ -28,23 +21,10 @@ public class IClientConnection extends IConnection implements ClientConnection {
     protected IClientConnection(IConnectionManager connectionManager, String serverAddress, int port) throws Exception {
         super(connectionManager, serverAddress, port);
 
-        IClientConnection instance = this;
-
         bootstrap.group(group)
                 .channel(NioSocketChannel.class)
-                .handler(new ChannelInitializer<SocketChannel>() {
-                    @Override
-                    public void initChannel(SocketChannel socketChannel) throws Exception {
-                        connectionHandler = new ConnectionHandler(instance);
-
-                        socketChannel.pipeline().addLast(
-                                new ObjectEncoder(),
-                                new ObjectDecoder(ClassResolvers.cacheDisabled(null)),
-                                connectionHandler,
-                                new IdleStateHandler(5000, 0, 0),
-                                new ReconnectHandler(instance));
-                    }
-                }).remoteAddress(serverAddress, port);
+                .handler(new StandardInitializer(this))
+                .remoteAddress(serverAddress, port);
 
         init();
     }
--- src/main/java/com/iKeirNez/packetapi/implementation/connection/IServerConnection.java
+++ src/main/java/com/iKeirNez/packetapi/implementation/connection/IServerConnection.java
@@ -1,18 +1,11 @@
 package com.iKeirNez.packetapi.implementation.connection;
 
 import com.iKeirNez.packetapi.api.connection.ServerConnection;
-import com.iKeirNez.packetapi.implementation.handlers.ConnectionHandler;
-import com.iKeirNez.packetapi.implementation.handlers.ReconnectHandler;
+import com.iKeirNez.packetapi.implementation.handlers.StandardInitializer;
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.ChannelInitializer;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
-import io.netty.handler.codec.serialization.ClassResolvers;
-import io.netty.handler.codec.serialization.ObjectDecoder;
-import io.netty.handler.codec.serialization.ObjectEncoder;
-import io.netty.handler.timeout.IdleStateHandler;
 
 /**
  * Created by iKeirNez on 04/04/2014.
@@ -26,23 +19,10 @@ public class IServerConnection extends IConnection implements ServerConnection {
     protected IServerConnection(IConnectionManager connectionManager, String clientAddress, int port) throws Exception {
         super(connectionManager, clientAddress, port);
 
-        IServerConnection instance = this;
-
         serverBootstrap.group(bossGroup, workerGroup)
                 .channel(NioServerSocketChannel.class)
-                .childHandler(new ChannelInitializer<SocketChannel>() {
-                    @Override
-                    public void initChannel(SocketChannel socketChannel) throws Exception {
-                        connectionHandler = new ConnectionHandler(instance);
-
-                        socketChannel.pipeline().addLast(
-                                new ObjectEncoder(),
-                                new ObjectDecoder(ClassResolvers.cacheDisabled(null)),
-                                connectionHandler,
-                                new IdleStateHandler(5000, 0, 0),
-                                new ReconnectHandler(instance));
-                    }
-                }).localAddress(clientAddress, port);
+                .childHandler(new StandardInitializer(this))
+                .localAddress(clientAddress, port);
 
         init();
     }
